7922bbb9733dcaf70df620f2467c33f27291e3ba
ArafatLsn <arafatsarkar22@gmail.com>
2023-12-27 15:27:46 +0600

    fixed

--- src/index.js
+++ src/index.js
@@ -4,12 +4,6 @@ import express from "express";
 const app = express();
 import { dbFunc } from "./db/index.js";
 
-// middlewares
-
-app.get("/api/v1/hello-express", (req, res) => {
-  res.send("express server response");
-  UserModel;
-});
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
